5948326adc4c4af9a1de9865d759ae3b4342d6d2
Nicholas Montaño <montanonic@gmail.com>
2016-06-06 17:15:30 -0700

    Add User models and migration

--- priv/repo/migrations/20160605195037_create_user.exs
+++ priv/repo/migrations/20160605195037_create_user.exs
@@ -4,10 +4,17 @@ defmodule Billing.Repo.Migrations.CreateUser do
   def change do
     create table(:users) do
       add :name, :string
+      # the email tied to their google account used to sign in
+      add :google_email, :string
+      # the address they prefer any emails to be sent to; if null, we default
+      # to emailing to their google address.
+      add :preferred_email, :string
       add :google_id, :string
+      add :access_token, :string
 
       timestamps
     end
+    create unique_index(:users, [:google_email])
 
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -3,7 +3,13 @@ defmodule Billing.User do
 
   schema "users" do
     field :name, :string
+    # the email tied to their google account used to sign in
+    field :google_email, :string
+    # the address they prefer any emails to be sent to; if null, we default
+    # to emailing to their google address.
+    field :preferred_email, :string
     field :google_id, :string
+    field :access_token, :string
 
     timestamps
   end
@@ -13,8 +19,9 @@ defmodule Billing.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :google_id])
-    |> validate_required([:name, :google_id])
+    |> cast(params,
+      ~w|name google_email preferred_email google_id access_token|a)
+    |> validate_required(~w|name google_email google_id access_token|a)
   end
 
   #def get_by_google_id(google_id) do
